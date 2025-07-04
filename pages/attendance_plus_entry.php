eb95377bb1891cd94a45d53dd19b561c1c979c61
Fikri Zumar <muhammad.fikri.azzumardi@gmail.com>
2025-07-04 10:52:20 +0700

    Memberikan permission di halaman entry

--- pages/attendance_plus_entry.php
+++ pages/attendance_plus_entry.php
@@ -3,6 +3,8 @@ $page_security = 'SA_HR_ATTENDANCE_PLUS';
 $path_to_root = "../../..";
 include($path_to_root . "/includes/session.inc");
 
+add_access_extensions();
+
 page(_($help_context = "Attendance Entry"));
 
 include_once($path_to_root . "/includes/ui.inc");
